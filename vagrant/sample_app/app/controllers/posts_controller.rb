413dddb7c165d89511f093e040acfa8adbaaf6c1
にしゆうき <nishiyuki@nishiyuukinoMacBook-Air.local>
2019-09-07 14:34:33 +0900

    [Finish]3-6

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -1,21 +1,22 @@
 class PostsController < ApplicationController
- 
+
     def new
          @post = Post.new
     end
-    
     # 以下を追加
     def create
-        # ストロングパラメーターを使用
-         post = Post.new(post_params)
-        # DBへ保存する
-         post.save
-        # トップ画面へリダイレクト
-        redirect_to '/top'
+        post = Post.new(post_params)
+        post.save # DBへ保存する
+        redirect_to post_path(post.id) # 詳細画面へリダイレクト
     end
- 
+    def index
+      @posts = Post.all
+    end
+    def show
+      @post = Post.find(params[:id])
+    end
+
     private
-   
     def post_params
         params.require(:post).permit(:title, :body)
     end
